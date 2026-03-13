91b99dc02f06ab45e2a86b9b0b8071706f3632ac
Oscar <pandemilconqueso@gmail.com>
2026-03-13 09:51:39 -0500

    ajustes de icono de grano de cafe

--- components/CoffeeBeanIcon.tsx
+++ components/CoffeeBeanIcon.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function CoffeeBeanIcon({ className = "w-6 h-6" }: { className?: string }) {
   return (
-    <svg 
-      viewBox="0 0 24 24" 
-      fill="currentColor" 
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
       className={className}
       xmlns="http://www.w3.org/2000/svg"
     >
